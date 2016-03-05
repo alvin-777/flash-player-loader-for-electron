c8aa6122b8575f3d6c8f2d439dd7309ee2597c33
Alvin <alvin-777@outlook.com>
2016-03-05 21:05:51 +1000

    Added an index page to the example app, which is slightly faster than loading the adobe about page

--- test/index.js
+++ test/index.js
@@ -10,10 +10,10 @@ app.on('window-all-closed', function() {
 });
 
 var flashLoader = require('..');
-var ppapi_flash_path = path.join(__dirname, flashLoader.FLASH_PLAYER_FILENAME);
 flashLoader.debug(true,
   console.log.bind(console, '[INFO] %s'.cyan),
   console.error.bind(console, '[ERROR] %s'.bold.red));
+var ppapi_flash_path = path.join(__dirname, flashLoader.FLASH_PLAYER_FILENAME);
 flashLoader.addSource(ppapi_flash_path);
 flashLoader.addSource('@chrome');
 flashLoader.addSource('@system');
@@ -21,9 +21,13 @@ flashLoader.load();
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
-    'width': 800,
-    'height': 600,
+    'width': 416,
+    'height': 380,
     'web-preferences': {'plugins': true}
   });
-  mainWindow.loadURL('http://www.adobe.com/software/flash/about/');
+  var url;
+  url = 'file://' + __dirname + '/index.html';
+  // url = 'http://www.adobe.com/software/flash/about/';
+  // url = 'https://helpx.adobe.com/flash-player.html';
+  mainWindow.loadURL(url);
 });
