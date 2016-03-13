65ffc11ece902c5c58d66ab9bb60665f2b8cd0af
Alvin <alvin-777@outlook.com>
2016-03-14 09:52:52 +1000

    Updated the example test app

--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
 var app = require('electron').app;
 var BrowserWindow = require('electron').BrowserWindow;
-var path = require('path');
 require('colors');
 
 var mainWindow = null;
@@ -9,24 +8,36 @@ app.on('window-all-closed', function() {
   app.quit();
 });
 
+// Load the module
 var flashLoader = require('..');
+// Using custom log functions to add extra prefixes and colours.
 flashLoader.debug(
   console.log.bind(console, '[INFO] %s'.cyan),
   console.error.bind(console, '[ERROR] %s'.bold.red));
-var ppapi_flash_path = path.join(__dirname);
-flashLoader.addSource(ppapi_flash_path, '1.2.3.4');
-flashLoader.addSource('@chrome');
+// Try to load Flash Player in the directory 'path_to_this_module/test/' first.
+// To make it work you need to copy the Flash Player to the 'test' directory,
+// or change the path to where your Flash Player file locates.
+// If that fails, then try to find the system plug-in (OS X only).
+// And then try to find the Chrome integrated plug-in (OS X only).
+var pathToDirContainsFlashPlayer = __dirname;
+flashLoader.addSource(pathToDirContainsFlashPlayer, '1.2.3.4');
 flashLoader.addSource('@system');
+flashLoader.addSource('@chrome');
 flashLoader.load();
+
+// Alternatively, you can chain the methods together.
+// require('..').debug().addSource('@system').addSource('@chrome').load();
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
     'width': 416,
     'height': 380,
-    'web-preferences': {'plugins': true}
+    'web-preferences': {'plugins': true}  // Do not forget to add this option
+                                          // when creating BrowserWindow
   });
   var url;
   url = 'file://' + __dirname + '/index.html';
+  // Try the other links out!
   // url = 'http://www.adobe.com/software/flash/about/';
   // url = 'https://helpx.adobe.com/flash-player.html';
   mainWindow.loadURL(url);
